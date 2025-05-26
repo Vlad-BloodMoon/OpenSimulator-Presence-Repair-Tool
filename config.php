8bb97e4c608980e23b74e42140b66fdf36af2743
Mickaël <131853826+Vlad-BloodMoon@users.noreply.github.com>
2025-05-26 10:53:09 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'robust',
+    'db_user' => 'your_mysql_user',
+    'db_pass' => 'your_mysql_password',
+    'log_path' => __DIR__ . '/repair_ghosts.log'
+];
